8146386fe761fbe43c121188e72724a46d4d2b40
Ayushi <ayushi2530@gmail.com>
2026-02-08 00:10:14 +0800

    new files added

---
+++ 2026/Week01/Mini_Tasks/45_grep_use.sh
@@ -0,0 +1,15 @@
+grep, awk, sed → log parsing & data extraction
+
+cut, sort, uniq, tr, wc
+
+find, xargs
+
+ps, top, htop, free, df, du
+
+tar, gzip, zip
+
+curl, wget
+
+ssh, scp, rsync
+
+crontab
