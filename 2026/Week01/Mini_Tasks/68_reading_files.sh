9043f3e8041f81c131f4c564c1b1e257e0d04998
Ayushi <ayushi2530@gmail.com>
2026-02-09 12:17:20 +0800

    deleted files

---
+++ 2026/Week01/Mini_Tasks/68_reading_files.sh
@@ -0,0 +1,11 @@
+<<COMMENT
+File Operations & Text Processing Basics
+- View file content
+- View file with line numbers
+- View command history
+- Show last 10 executed commands
+COMMENT
+
+#!/bin/bash
+
+echo "Displaying the content of '68_reading_files.sh':"
